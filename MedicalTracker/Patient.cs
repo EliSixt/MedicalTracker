0eab92f7af89f945e96477b0471879a0a3932506
Elias <elias_sixtos@outlook.com>
2021-11-08 19:03:42 -0500

    Add project files.

---
+++ MedicalTracker/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalTracker
+{
+    public class Patient
+    {
+        //A lot of information needs an input, look into how to obtain data from EHR(Electronic Health Records)
+        //Hospitals use different types of EHR registries, look into the UofL hospitals in local area.
+        public string Name { get; set; }//firstname and lastname ?
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; } //Its own separate class
+        public string Allergies { get; set; }//Its own separate class
+        public string EmergencyContacts { get; set; }//Its own separate class
+        public string DailyMedication { get; set; }//Its own separate class, history of past medicine
+        public string EmergencyMedication { get; set; }//Its own separate class
+        public string Appointments { get; set; }//Its own separate class
+        public string MedicalCondition { get; set; }//Its own separate class
+        public string Symptoms { get; set; }//Its own separate class
+        public string SpecialNeeds { get; set; }//Its own separate class
+        public string Caretakers { get; set; }//Its own separate class
+        public string ContactInfo { get; set; }//Its own separate class? Comprised of address, phoneNumber, email?
+
+    }
+}
---
+++ MedicalTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
