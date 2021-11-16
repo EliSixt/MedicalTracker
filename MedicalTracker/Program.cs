590cba86b65b33e9fe728eb01bdf07bf203321b1
Elias <elias_sixtos@outlook.com>
2021-11-15 22:14:34 -0500

    Started to hardCode some info for patient to start methods for app.

--- MedicalTracker/Program.cs
+++ MedicalTracker/Program.cs
@@ -6,7 +6,29 @@ namespace MedicalTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static Patient TestPatient()
+            {
+                Patient practicePatient = new Patient();
+
+                practicePatient.PatientInfo.Name.FirstName = "Larry";
+                practicePatient.PatientInfo.Name.MiddleInitial = 'Z';
+                practicePatient.PatientInfo.Name.LastName = "Quincey";
+
+                practicePatient.PatientInfo.MobilePhoneNum = "502-800-8080";
+                practicePatient.PatientInfo.Email = "LarryIsCool@gmail.com";
+                practicePatient.PatientInfo.Address.BuildingNumber = 8080;
+                practicePatient.PatientInfo.Address.StreetName = "CoolStreet Blvd";
+                practicePatient.PatientInfo.Address.City = "louisville";
+                practicePatient.PatientInfo.Address.State = "kentucky";
+                practicePatient.PatientInfo.Address.ZIPCode = 41234;
+
+                //emergency calls
+                //practicePatient.EmergencyContacts.Add(new ContactInfo { new Name { FirstName = "Joe"} });
+
+
+
+                return practicePatient;
+            }
         }
     }
 }
