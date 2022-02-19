2682fb1fbbe30b72e71002e01ed2d86fc0dd93fa
Elias <elias_sixtos@outlook.com>
2022-02-19 01:23:35 -0500

    Added notes and started making GetDailyMedicines component

--- MedicalTracker/DailyMedicine.cs
+++ MedicalTracker/DailyMedicine.cs
@@ -8,7 +8,7 @@ namespace MedicalTracker
 {
     public class DailyMedicine
     {
-        public Medicine Medicine { get; set; } = new Medicine();
+        public Medicine Medicine { get; set; } = new();
         public string DoseMg { get; set; }//medication taken at one time
         public string FrequencyOfDose { get; set; }//frequency of doses daily
         public TimeSpan TimeSpanOfDose { get; set; }
