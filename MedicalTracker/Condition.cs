ad0a2edf1f666ede327f5e3f98491b02d7874fe8
Elias Sixtos <elias_sixtos@outlook.com>
2022-05-18 22:41:12 -0400

    Overrided the .Equals function, uses the comditionName to compare two condition objects and determine if they're similiar.
    Override the == and != operators with the .equals function to see if two condition objects are the same.

--- MedicalTracker/Condition.cs
+++ MedicalTracker/Condition.cs
@@ -2,7 +2,7 @@
 
 namespace MedicalTracker
 {
-     public class Condition
+    public class Condition
     {
         public List<Symptom> Symptoms { get; set; }//symptoms associated with this condition
         public string UserID { get; set; }
@@ -21,6 +21,39 @@ namespace MedicalTracker
         public override string ToString()
         {
             return $"name = {ConditionName}, severity = {Severity}, sex = {Sex}, age = {Age}, country = {Country}, userID = {UserID}";
+        }
+        /// <summary>
+        /// Uses the ConditionName from two Condition objects and returns if the two objects are the same or not.
+        /// </summary>
+        /// <param name="obj">Other object being compared</param>
+        /// <returns>Bool</returns>
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            return ConditionName.ToLower() == ((Condition)obj).ConditionName.ToLower();
+        }
+        /// <summary>
+        /// Uses the .Equals function to determine if two Condition objects are equal to one another.
+        /// </summary>
+        /// <param name="condition1"></param>
+        /// <param name="condition2"></param>
+        /// <returns>Bool</returns>
+        public static bool operator ==(Condition condition1, Condition condition2)
+        {
+            return condition1.Equals(condition2);
+        }
+        /// <summary>
+        /// Uses the .Equals function to determine if two condition objects are equal to one another.
+        /// </summary>
+        /// <param name="condition1"></param>
+        /// <param name="condition2"></param>
+        /// <returns>Bool</returns>
+        public static bool operator !=(Condition condition1, Condition condition2)
+        {
+            return condition1.Equals(condition2);
         }
     }
 }
