59d169e83ec391ee4b07c5984a01a518dbc67ca9
Elias <elias_sixtos@outlook.com>
2022-07-30 23:13:19 -0400

    Converting HasDuplicate() method from the dataservice into an extension method inside the ExtensionMethods static class.

--- MedicalTrackerBlazorApp/Data/ExtensionMethods.cs
+++ MedicalTrackerBlazorApp/Data/ExtensionMethods.cs
@@ -2,6 +2,32 @@
 {
     public static class ExtensionMethods
     {
-        
+        /// <summary>
+        /// Checks list if there's an item thats the same as the provided object.
+        /// Null checks.
+        /// Checks if the list's type and the object's type are the same.
+        /// </summary>
+        /// <param name="value">The List of items</param>
+        /// <param name="objToCheck">Object to compare</param>
+        /// <returns>Boolean</returns>
+        public static bool HasDuplicate<T>(this List<T> value, T objToCheck)
+        {
+            if (objToCheck is null && value is null)
+            {
+                return true;
+            }
+            if (objToCheck is null || value is null)
+            {
+                return false;
+            }
+            foreach (T obj in value)
+            {
+                if (obj != null && obj.Equals(objToCheck))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
