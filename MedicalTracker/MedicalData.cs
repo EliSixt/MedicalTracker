27170f4e2e6295554e148d22fbfa52af7823badb
Elias <Elias_sixtos@outlook.com>
2023-08-27 21:34:48 -0400

    Started a new component called MedicalData.
    MedicalData will be structured around one CSV local file, made from all the medical data gathered. It's going to help organize and support the component ClippyDiagnosisHelper.

---
+++ MedicalTracker/MedicalData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalTracker
+{
+    internal class MedicalData
+    {
+        //constructor
+        public MedicalData()
+        {
+        }
+
+        //symptoms are indicators of a disease or condition, while a disease is a specific pathological process,
+        //and a condition is a more general term that encompasses both diseases and other health-related issues.
+
+
+        public List<Condition> Disease { get; set; }
+    }
+}
